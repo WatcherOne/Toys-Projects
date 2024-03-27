2c1d072939bdb28fc0e5fe59f93229a2786f0e23
觅安 <286154864@qq.com>
2024-03-27 20:41:27 +0800

    Add files via upload

---
+++ config.api.js
@@ -0,0 +1,10 @@
+// 查询是否签到
+export const isSignIn = 'https://api.juejin.cn/growth_api/v1/get_today_status'
+// 签到接口
+export const signIn = 'https://api.juejin.cn/growth_api/v1/check_in'
+// 免费抽奖次数查询
+export const freeCheck = 'https://api.juejin.cn/growth_api/v1/lottery_config/get'
+// 抽奖接口
+export const drawAPI = 'https://api.juejin.cn/growth_api/v1/lottery/draw'
+// 获得矿石数量
+export const getPointCount = 'https://api.juejin.cn/growth_api/v1/get_cur_point'
