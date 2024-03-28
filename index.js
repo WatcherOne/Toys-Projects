14a52f2fa978d9f52cb65602d5856d5cc175c7f5
觅安 <286154864@qq.com>
2024-03-28 15:41:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const LOG_MSG = {
     remainedPoint: 0,  // 剩余矿石数
     freeDrawTimes: 0,  // 免费抽奖次数
     drawResult: null,  // 抽奖结果
-    emailStatus: null  // 邮件发送状态：true: 发送成功，false: 发送失败
+    emailStatus: null, // 邮件发送状态：true: 发送成功，false: 发送失败
+    error: null        // 错误信息
 }
 
 // 延迟去获取免费抽奖次数
@@ -35,19 +36,15 @@ function handleEmailMessage () {
     return `当前日期: ${getCurrentDate()} ${getCurrentTime()}\n签到情况: ${signStatus}\n矿石数: ${pointCount}\n抽奖情况: ${drawStatus}`
 }
 
-async function toLog (msg) {
-    const logMsg = msg || JSON.stringify(LOG_MSG)
-    try {
-        fs.appendFileSync('./logs.txt', `${logMsg}@\n`)
-    } catch (err) {
-        console.log(`日志写入失败: ${err}`)
-    }
+async function toLog () {
+    fs.appendFileSync('./logs.txt', `${JSON.stringify(LOG_MSG)}@\n`)
 }
 
 async function main () {
     LOG_MSG.currentTime = `${getCurrentDate()} ${getCurrentTime()}`
     if (!COOKIE) {
-        await toLog('用户不存在，脚本执行失败')
+        LOG_MSG.error = '用户不存在，脚本执行失败'
+        await toLog()
         return
     }
     const isSignedObj = await checkIsSignedIn()
