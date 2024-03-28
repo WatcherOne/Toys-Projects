5f812a0eed09e33c4676267e24c2e7551a042fed
zhubo <286154864@qq.com>
2024-03-28 22:59:59 +0800

    fix: render page layout

--- public/index.js
+++ public/index.js
@@ -5,15 +5,19 @@
         'Content-Type': 'application/json',
     }
 
-    const isSigned = false  // 是否签到
-    const remianedPoint = 0 // 剩余矿石数
-
     function init () {
         fetch('/api/getCommon', {
             method: 'GET',
             headers: HEADERS,
         }).then(res => res.json()).then(res => {
-            console.log(res)
+            const { code, data } = res
+            if (code === 200) {
+                // 是否签到 & 剩余矿石数
+                const { isSigned, remianedPoint } = data || {}
+                const msg = isSigned === true ? '今日已签到' : (isSigned === false ? '今日未签到' : '登陆已过期')
+                document.getElementById('is-signed').innerHTML = msg
+                document.getElementById('remained-point').innerHTML = remianedPoint || 0
+            }
         })
     }
 
@@ -22,8 +26,14 @@
         fetch('/api/start', {
             method: 'GET',
             headers: HEADERS
-        }).then(res =>  res.json()).then(data => {
-            console.log(data)
+        }).then(res =>  res.json()).then(res => {
+            const { code, data } = res
+            if (code === 200) {
+                alert('启动成功')
+                getProcess()
+            } else {
+                alert(data)
+            }
         })
     }
 
@@ -32,8 +42,14 @@
         fetch('/api/stop', {
             method: 'GET',
             headers: HEADERS
-        }).then(res =>  res.json()).then(data => {
-            console.log(data)
+        }).then(res =>  res.json()).then(res => {
+            const { code, data } = res
+            if (code === 200) {
+                alert('停止成功')
+                getProcess()
+            } else {
+                alert(data)
+            }
         })
     }
     
@@ -42,15 +58,36 @@
             method: 'GET',
             headers: HEADERS
         }).then(res =>  res.json()).then(data => {
-            handleProcess(data)
+            handleProcess(data.data || '')
         })
     }
 
     const $processDom = document.getElementById('process-content')
     function handleProcess (data) {
-        $processDom.innerHTML = data.data
+        const strArr = data.split('│')
+        strArr.pop()
+        const times = Math.ceil(strArr.length / 14)
+        let result = []
+        for (let i = 0; i < times; i++) {
+            let rowItem = []
+            for (let j = 1; j < 14; j++) {
+                const index = i * 14 + j
+                rowItem.push(strArr[index] ? strArr[index].trim() : '')
+            }
+            result.push(rowItem)
+        }
+        let tr = ''
+        result.forEach((arr, index) => {
+            tr += '<tr>'
+            arr.forEach(item => {
+                tr += `${index ? '<td>' : '<th>'}${item}${index ? '</td>' : '</th>'}`
+            })
+            tr +=  '</tr>'
+        })
+        $processDom.innerHTML = tr
     }
 
+    document.getElementById('update-log').addEventListener('click', getLogs)
     function getLogs () {
         fetch('/api/getLogs', {
             method: 'GET',
@@ -70,23 +107,56 @@
                 const obj = str ? JSON.parse(str) : {}
                 str && logList.push(obj)
             })
-            appendLogToHtml(logList)
+            appendLogToHtml(logList.reverse())
         } catch (error) {
             console.log('error: ', error)
         }
     }
 
+    const LOG_MSG = {
+        currentTime: '',   // 当前时间
+        signStatus: null,  // 是否签到标识: true: 已签到  false: 未签到
+        signResult: null,  // 签到结果：true: 签到成功，false: 签到失败，其它签到失败原因 
+        remainedPoint: 0,  // 剩余矿石数
+        freeDrawTimes: 0,  // 免费抽奖次数
+        drawResult: null,  // 抽奖结果
+        emailStatus: null, // 邮件发送状态：true: 发送成功，false: 发送失败
+        error: null        // 错误信息
+    }
+
     const $logListDom = document.getElementById('log-list')
     function appendLogToHtml (list) {
+        $logListDom.innerHTML = ''
         list.forEach(item => {
-            const { currentTime, remainedPoint } = item
+            const { currentTime, remainedPoint, signResult, freeDrawTimes, drawResult, signStatus, emailStatus, error } = item
             const $div = document.createElement('div')
-            $div.innerHTML = `
-                <div>${currentTime}</div>
-                <div>${remainedPoint}</div>
+            $div.classList.add('one-log')
+            $div.innerHTML = error
+            ? `
+                <div class="top error">Error:</div>
+                <div>${currentTime}---</div>
+                <div class="error">${error}</div>
+            `
+            : `
+                <div class="top">Success:</div>
+                <div>${currentTime}---</div>
+                <div>${signResult ? (getSignResult(signResult)) : (signStatus ? '今日已签到' : '今日未签到')}---</div>
+                <div>剩余矿石数: <span class="count">${remainedPoint}</span>---</div>
+                <div>${freeDrawTimes ? (drawResult || '') : '没有免费抽奖次数'}---</div>
+                <div>${emailStatus ? '邮件发送成功' : '邮件发送失败'}</div>
             `
             $logListDom.appendChild($div)
         })
+    }
+
+    function getSignResult (signResult) {
+        if (signResult === 'true') {
+            return '签到成功'
+        } else if (signResult === 'false') {
+            return '签到失败'
+        } else {
+            return signResult
+        }
     }
 
     init()
